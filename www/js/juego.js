7b50fe61ad11bf54885fb9c7b11da7fae5bdffda
jesus-molina <tmpjmg@gmail.com>
2017-02-04 13:33:31 +0100

    añadir incremento de dificultad al tocar objetivo

--- www/js/juego.js
+++ www/js/juego.js
@@ -1,6 +1,7 @@
 var app={
   inicio: function(){
     DIAMETRO_BOLA = 50;
+    dificultad = 0;
     velocidadX = 0;
     velocidadY = 0;
     puntuacion = 0;
@@ -36,7 +37,7 @@ var app={
       game.load.image('bola', 'assets/bola.png');
       game.load.image('objetivo', 'assets/objetivo.png');
     }
-    
+
     function create() {
       scoreText = game.add.text(16, 16, puntuacion, { fontSize: '100px', fill: '#757676' });
       
@@ -52,8 +53,9 @@ var app={
     }
 
     function update(){
-      bola.body.velocity.y = (velocidadY * 300);
-      bola.body.velocity.x = (velocidadX * (-1 * 300));
+      var factorDificultad = (300 + (dificultad * 100));
+      bola.body.velocity.y = (velocidadY * factorDificultad);
+      bola.body.velocity.x = (velocidadX * (-1 * factorDificultad));
       
       game.physics.arcade.overlap(bola, objetivo, app.incrementaPuntuacion, null, this);
     }
@@ -73,6 +75,10 @@ var app={
 
     objetivo.body.x = app.inicioX();
     objetivo.body.y = app.inicioY();
+
+    if (puntuacion > 0){
+      dificultad = dificultad + 1;
+    }
   },
 
   inicioX: function(){
